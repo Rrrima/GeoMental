7c677c8dfceb4285b8dda117cdd0606061e8eabb
Rrrima <rima_cyn@126.com>
2018-12-12 22:53:40 +0800

    narrow sample size

--- pointData.py
+++ pointData.py
@@ -46,10 +46,31 @@ class PointFeatures(object):
 		ref_map = self._map
 		value = self._rgb
 		total_dis = 0
-		for x in range(self._x-radius, self._x+radius):
-			for y in range(self.y-radius, self._y+radius):
+		for x in range(self._x-radius, self._x+radius+1):
+			for y in range(self.y-radius, self._y+radius+1):
 				total_dis += eu_distance(ref_map.get_pixel_value(x,y),value)	
 		return total_dis
+
+	def _cal_total_std(self,radius=1):
+		ref_map = self._map
+		matrix = []
+		for x in range(self._x-radius, self._x+radius+1):
+			for y in range(self.y-radius, self._y+radius+1):
+				matrix.append(ref_map.get_pixel_value(x,y))
+		return get_std(matrix)
+
+	def _cal_max_min_dis(self,radius=1):
+		ref_map = self._map
+		dis_list = []
+		value = self._rgb
+		for x in range(self._x-radius, self._x+radius+1):
+			for y in range(self.y-radius, self._y+radius+1):
+				if ((x==self._x) & (y==self._y)):
+					continue
+				else:
+					dis_list.append(eu_distance(ref_map.get_pixel_value(x,y),value))
+		return [np.max(dis_list),np.min(dis_list)]
+
 
 	# make sure the radius wont exceed image boundaries	
 	def _create_features(self):
@@ -58,19 +79,41 @@ class PointFeatures(object):
 		vector.extend(bands)
 		bands_std = get_std(bands)
 		vector.append(bands_std)
-		vector.append(self._cal_rgb_dis(2))
+		rgb_dis = []
+		rgb_std = []
+		for i in range(1,5):
+			rgb_dis.append(self._cal_rgb_dis(i))
+		for i in range(1,4):
+			rgb_std.append(self._cal_total_std(i))
+		vector.extend(rgb_dis)
+		vector.append(get_std(rgb_dis))
+		vector.extend(rgb_std)
+		vector.append(get_std(rgb_std))
+		vector.extend(self._cal_max_min_dis())
+
 
 		return vector
 
 
 
 class SampleSet(object):
-	def __init__(self, dataPath, mapPath):
+	def __init__(self, dataPath, mapPath, boundary=None):
 		self._map = GeoMap(mapPath)
 		self._origin_points = get_sampling_points(dataPath)
-		self._size = len(self._origin_points)
+		if boundary is None:
+			self._bleft = 0
+			self._bright = self._map.xsize
+			self._bbottom = 0
+			self._btop = self._map.ysize
+		else:
+			self._bleft = boundary[0]
+			self._bright = boundary[1]
+			self._bbottom = boundary[2]
+			self._btop = boundary[3]
+			print(boundary)
 		self._featured_points = self._create_points()
 		self._rgb_dbscan_labels = self._clustering_DBSCAN()
+		self._size = len(self._featured_points)
 
 	@property
 	def size(self):
@@ -93,16 +136,23 @@ class SampleSet(object):
 		ph = ref_map.pixel_height
 		return int((x-x0)/pw),int((y-y0)/ph)
 
+	def _is_in_boundary(self,x,y):
+		l = self._bleft
+		r = self._bright
+		b = self._bbottom
+		t = self._btop
+		return ((x>l) & (x<r) & (y>b) & (y<t))
+
 	def _create_points(self):
 		points = []
-		boundx = self._map.xsize
-		boundy = self._map.ysize
 		for i in range(self.size):
 			this_point = self._origin_points[i]
 			converted_cord = self._convert_point(this_point[0],this_point[1])
 			this_point[0] = converted_cord[0]
 			this_point[1] = converted_cord[1]
-			if ((this_point[0]>0) & (this_point[1]>0) & (this_point[0]<boundx) & (this_point[1]<boundy)):
+			print(this_point[0],this_point[1])
+			if (self._is_in_boundary(this_point[0],this_point[1])):
+				print(1)
 				points.append(PointFeatures(this_point,self._map))
 		return points
 
--- test.py
+++ test.py
@@ -2,19 +2,26 @@
 # REMOVE it from the final project
 import pprint
 import numpy as np
+import matplotlib.pyplot as plt
 np.set_printoptions(threshold=np.inf)
 pp = pprint.PrettyPrinter(indent=4)
 
 FILEPATH_SAMPLE = 'data/top_data.xls'
 # USE ABSOLUTE PATH!
 FILEPATH_MAP = '/Users/rima/GeoMental/zx_field_data/map.tif'
-
+BOUNDARY = [8000,13400,3800,5850]
 
 from pointData import PointFeatures, SampleSet
 
 if __name__ == "__main__":
-	my_set = SampleSet(FILEPATH_SAMPLE, FILEPATH_MAP)
+	my_set = SampleSet(FILEPATH_SAMPLE, FILEPATH_MAP, BOUNDARY)
 	my_data = my_set.points
+	# plot points
+	xlist = [data.x for data in my_data]
+	ylist = [data.y for data in my_data]
+	clist = [tuple([x/255 for x in data.rgb]) for data in my_data]
 	for data in my_data:
 		print(data.vector,data.Cr)
 	print (my_set.rgb_labels)
+	plt.scatter(xlist, ylist, c=clist, alpha = 0.6)
+	plt.show()
